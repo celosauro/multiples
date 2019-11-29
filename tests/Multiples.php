a7f0cf73091a36a721d12dfbcf78c59c5a5fbdba
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2019-11-28 22:32:32 -0300

    Add tests

---
+++ tests/Multiples.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Multiples\{Factory, AbstractMultiple, MultipleInterface, NotMultiple};
+
+class Multiples extends TestCase
+{
+    public function testInstanceOf(): void
+    {
+        $this->assertInstanceOf(AbstractMultiple::class, Factory::create(rand()));
+        $this->assertInstanceOf(MultipleInterface::class, Factory::create(rand()));
+    }
+
+    public function testIsValidOutput(): void
+    {
+        $this->expectException(\ArgumentCountError::class);
+        new NotMultiple();
+    }
+}
+
